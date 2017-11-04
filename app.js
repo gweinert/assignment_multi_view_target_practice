649cb4cbf85ecedc80a040b794769488347c4aa6
Garrett <garrettweinert@gmail.com>
2017-11-04 11:29:42 -0700

    child state

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ var app = angular.module('TargetPractice', ['ui.router']);
 app.config(['$stateProvider', '$urlRouterProvider',
 	function($stateProvider, $urlRouterProvider) {
 
+		$urlRouterProvider.otherwise('/');
+
 		$stateProvider
 
 		.state('parent', {
 			url: '',
 			views: {
 				'': {
-					template: '<p>Targeting unnamed view from parent state</p>',
+					//template: '<p>Targeting unnamed view from parent state</p>',
+					templateUrl: './templates/child.html',
 				},
 				'named-parent': {
 					template: '<p>Targeting named view from parent state</p>',
@@ -23,7 +26,18 @@ app.config(['$stateProvider', '$urlRouterProvider',
 		.state('parent.child', {
 			url: '/child',
 			views: {
-
+				'': {
+					template: '<p>Targeting unnamed child view from child state</p>'
+				},
+				'named-child': {
+					template: '<p>Targeting named child view from child state</p>'
+				},
+				'named-parent@': {
+					template: '<p>Targeting named parent view from child state</p>',
+				},
+				'main-header@': {
+					template: '<h2>Targeting header from child state</h2>',
+				},
 			},
 		})
 	
